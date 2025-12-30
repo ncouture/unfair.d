2ade3046ce34638bd9321739d4cd81f8eb98eaa3
Nicolas Couture <nicolas@stormvault.net>
2025-12-30 15:34:40 -0500

    chore(architecture): add new files

---
+++ site-lisp/init-local.el
@@ -0,0 +1,10 @@
+;;;  -*- lexical-binding: t -*-
+;;; init-local.el --- customization overloading packages found in the main /lisp directory.
+;;; Commentary:
+;;; loads user-specific packages located in /user-lisp/
+;;; Code:
+
+(add-to-list 'load-path (expand-file-name "user-lisp" user-emacs-directory))
+
+(provide 'init-local)
+;;; init-local ends here.(add-to-list 'load-path (expand-file-name "user-lisp" user-emacs-directory))
