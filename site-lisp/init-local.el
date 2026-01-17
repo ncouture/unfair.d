9f550aeb62e0970272e0735b2df2b4f6af297fac
Nicolas Couture <nicolas@stormvault.net>
2026-01-17 00:58:16 -0500

    refactor(user-lisp): move auto-loading logic to init.el to prevent shadowing

--- site-lisp/init-local.el
+++ site-lisp/init-local.el
@@ -4,12 +4,7 @@
 ;;; loads user-specific packages located in /user-lisp/
 ;;; Code:
 
-(add-to-list 'load-path (expand-file-name "user-lisp" user-emacs-directory))
-
-(let ((user-lisp-dir (expand-file-name "user-lisp" user-emacs-directory)))
-  (when (file-directory-p user-lisp-dir)
-    (dolist (file (directory-files user-lisp-dir nil "^init-.*\\.el$"))
-      (require (intern (file-name-sans-extension file))))))
+;; Add your personal tweaks here
 
 
 
